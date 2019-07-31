f46cd74cc6fdb437b4d7f3682ffe63218c7a6b91
Samuel Kurniawan <kurniawan87@gmail.com>
2019-07-31 13:29:43 -0700

    adding phone code for countries
    British Indian Ocean Territory, Curacao, Guam, Eritrea, Isle of Man, Ivory Coast, Jersey, Mayotte, Niue, Northern Mariana Islands, Pitcairn, Saint Barthelemy, Saint Martin, Sint Maarten, Tonga, U.S. Virgin Islands, Vanuatu are missing. (side note: nigeria, haiti, USSR from the front end?)

--- emergency_app/models/nation.py
+++ emergency_app/models/nation.py
@@ -4,6 +4,7 @@ from django.db import models
 class Nation(models.Model):
     id = models.CharField(db_column='NATION_ID', max_length=4, primary_key=True)
     value = models.CharField(db_column='NATION_VALUE', max_length=120, null=True)
+    phone_code = models.CharField(db_column='NATION_PHONE_CODE', max_length=16, null=True)
 
     class Meta:
         db_table = 'NATION'
